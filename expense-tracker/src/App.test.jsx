2ae6eb984b781ebf99fb4153d04eb3f5a1d601f7
Machame1 <machame22@gmail.com>
2026-06-24 15:46:14 +0530

    change

--- expense-tracker/src/App.test.jsx
+++ expense-tracker/src/App.test.jsx
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom/vitest";
 import { afterEach, expect, test, vi } from "vitest";
 import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
